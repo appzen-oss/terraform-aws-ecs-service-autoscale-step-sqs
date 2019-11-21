c2c406041c1759479ce679076ba5e514ecd8c712
wesleung <wes@appzen.com>
2019-11-21 14:31:13 -0800

    add service and cluster name to stuck ECS alarm

--- main.tf
+++ main.tf
@@ -164,6 +164,10 @@ resource "aws_cloudwatch_metric_alarm" "service_max_stuck" {
   alarm_actions             = ["${var.sns_stuck_alarm_arn}"]
   ok_actions                = ["${var.sns_stuck_alarm_arn}"]
   insufficient_data_actions = []
+  dimensions                = { 
+    ClusterName             = "${var.cluster_name}"
+    ServiceName             = "${var.service_name}"
+  }
 }
 
 resource "aws_cloudwatch_metric_alarm" "service_queue_high" {
